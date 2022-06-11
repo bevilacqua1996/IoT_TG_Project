8a155886b1d80bfa6a0de509cafe8891d49170c4
lucas <lucasset10@msn.com>
2022-06-11 13:57:07 -0300

    Modificando SF e BW / Armazenando pacotes do receiver

--- communication/sensorDataReceiver/sensorDataReceiver.ino
+++ communication/sensorDataReceiver/sensorDataReceiver.ino
@@ -1,3 +1,4 @@
+
 /*
  * Camada de comunicação LoRa e API
  * 
@@ -8,7 +9,9 @@
 #include "sensorDataJSON.h"
 #include "restMapping.h"
 #include "support.h"
+//#include <List.hpp>
 
+#define TIME_CODE 0
 #define ROTATIONS_CODE 1
 #define VOLTAGE_CODE 2
 #define ACC_X_CODE 3
@@ -16,15 +19,24 @@
 #define ACC_Z_CODE 5
 #define TEMPERATURE_CODE 6
 
+#define NUM_OF_PACKETS 7
+
 //#include <WiFi.h>
 
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
+String packets[NUM_OF_PACKETS];
+int packet_size = 0;
+int packetsReceived = 0;
+int pkg_id=-1;
+
+unsigned long tempo;
 
 extern const char* ssid;
 extern const char* password;
+
 
 void logo(){
   Heltec.display->clear();
@@ -33,47 +45,73 @@ void logo(){
 }
 
 void buildAndSendLoRaData(){
-    Serial.println(packet);
-//  String json = buildJSON(packet);
-   StaticJsonDocument<2048> doc;
-   deserializeJson(doc, packet);
-   int vf = doc["voltage_factor"];
-   Serial.print("Voltage factor: ");Serial.println(vf);
-  
-  int httpResponseCode;
-  
-  if(WiFi.status()== WL_CONNECTED){
-//    httpResponseCode = postData(json);
-     httpResponseCode = postData(packet);
-  } else {
-    httpResponseCode = 500;
+  //String json = buildJSON(packet);
+  //int httpResponseCode;
+  if(packetsReceived >= NUM_OF_PACKETS){
+    int httpResponseCode = sendJson();
+    displayData(httpResponseCode);
+    packetsReceived = 0;
   }
+}
 
-//  String data_type = getValue(packet, 0);
-//  String type_str;
-//  String data_str = getValue(packet, 1);
-//  if(data_type.toInt() == ROTATIONS_CODE) {
-//    type_str = "Rotations";
-//  } else if(data_type.toInt() == VOLTAGE_CODE) {
-//    type_str = "Voltage";
-//  } else if(data_type.toInt() == ACC_X_CODE) {
-//    type_str = "Xacc";
-//  } else if(data_type.toInt() == ACC_Y_CODE) {
-//    type_str = "Yacc";
-//  } else if(data_type.toInt() == ACC_Z_CODE) {
-//    type_str = "Zacc";
-//  } else if(data_type.toInt() == TEMPERATURE_CODE) {
-//    type_str = "Temperature Cº";
-//  }
-//  
-//  Heltec.display->clear();
-//  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-//  Heltec.display->setFont(ArialMT_Plain_10);
-//  Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
-//  Heltec.display->drawString(0 , 40 , "Http Status Code: "+ String(httpResponseCode));
-//  Heltec.display->drawStringMaxWidth(0 , 26 , 128, type_str + " : " + data_str);
-//  Heltec.display->drawString(0, 0, rssi);  
-//  Heltec.display->display();
+void displayData(int httpResponseCode){
+//  int value =  doc["data"][0];
+  
+  //for(int i=0;i<packetsReceived;i++){
+  for(int i=0;i<NUM_OF_PACKETS;i++){
+    String m = "JSON [" + String(i) + "] = " + packets[i];
+    Serial.println(m);
+    
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, packets[i]);
+
+    int code = doc["code"];
+    int value = doc["data"][0];
+    //Serial.print("Value: ");Serial.println(value);
+    String data_str = String(value);
+    String type_str;
+    if(code == ROTATIONS_CODE) {
+      type_str = "Rotations";
+    } else if(code == VOLTAGE_CODE) {
+      type_str = "Voltage";
+    } else if(code == ACC_X_CODE) {
+      type_str = "Xacc";
+    } else if(code == ACC_Y_CODE) {
+      type_str = "Yacc";
+    } else if(code == ACC_Z_CODE) {
+      type_str = "Zacc";
+    } else if(code == TEMPERATURE_CODE) {
+      type_str = "Temp.: Cº";
+    }
+    
+    Heltec.display->clear();
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
+    Heltec.display->drawString(0 , 40 , "Http Status Code: "+ String(httpResponseCode));
+    Heltec.display->drawStringMaxWidth(0 , 26 , 128, type_str + " : " + data_str);
+    Heltec.display->drawString(0, 0, rssi);  
+    Heltec.display->display();
+    delay(100);
+  }
+}
+
+int sendJson(){
+  int httpResponseCode;
+  if(WiFi.status()== WL_CONNECTED){
+      //for(int i=0;i<sizeof(packets)/sizeof(packets[0]);i++){
+      for(int i=0;i<NUM_OF_PACKETS;i++){
+        httpResponseCode = postData(packets[i]);
+//        String m = "Packets[" + String(i) + "] = " + packets[i];
+//        Serial.println(m);
+        delay(300);
+//        delay(10);
+      }
+  }
+  else {
+      httpResponseCode = 500;
+  }
+  return httpResponseCode;
 }
 
 void cbk(int packetSize) {
@@ -81,9 +119,36 @@ void cbk(int packetSize) {
   packSize = String(packetSize,DEC);
   for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
   rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
-//  Serial.println(packet);
-  buildAndSendLoRaData();
+  //buildAndSendLoRaData();
+
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, packet);
+
+  int id = doc["id"];
+  int code = doc["code"];
+  //Serial.println(id); 
+  if(pkg_id == id || pkg_id==-1){
+    if(pkg_id==-1) pkg_id = id;    
+    //Serial.println(code); 
+    packetsReceived++;
+    packets[code] = packet;
+  }
+  else{
+    pkg_id = id;
+  }
 }
+
+
+void onReceive(int packetSize){
+  packet_size = packetSize;
+  if (packetSize) {
+    //tempo = millis();
+    //Serial.print("Packet size: ");Serial.println(packetSize); 
+    cbk(packetSize);
+    //Serial.print("Loop time: ");  Serial.println(millis()-tempo);  /* Serial.println(tempo); */
+  }
+}
+
 
 void wifiConfigure() {
   WiFi.begin(ssid, password);
@@ -117,13 +182,30 @@ void setup() {
   Heltec.display->drawString(0, 10, "Wait for incoming data...");
   Heltec.display->display();
   delay(1000);
-  LoRa.receive();
+
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(250E3);
   
+  LoRa.receive();
+  LoRa.onReceive(onReceive);
 }
 
 void loop() {
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) { Serial.println(packetSize); cbk(packetSize);  }
+//  int packetSize = LoRa.parsePacket();
+//  if (packetSize) {
+//    tempo = millis();
+//    Serial.print("Packet size: ");Serial.println(packetSize); 
+//    cbk(packetSize); 
+//    Serial.print("Loop time: ");  Serial.println(millis()-tempo);  /* Serial.println(tempo); */
+//    tempo = millis();
+//  }
   delay(10);
-//  delay(1000);
+////  delay(1000);
+  
+  if(packet_size){
+    //tempo = millis();
+    //Serial.println(millis());
+    buildAndSendLoRaData();
+    //Serial.print("Loop time: ");  Serial.println(millis()-tempo);
+  }
 }
--- communication/sensorDataSender/sensorDataSender.ino
+++ communication/sensorDataSender/sensorDataSender.ino
@@ -32,7 +32,7 @@
 #define ACC_Y_CODE 4
 #define ACC_Z_CODE 5
 #define TEMPERATURE_CODE 6
-#define FACTORS_CODE 'F'
+//#define FACTORS_CODE 'F'
 
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 
@@ -59,7 +59,7 @@ extern const char* ssid;
 extern const char* password;
 
 //const long DELAY_ = 60000;
-const long DELAY_ = 200;
+const long DELAY_ = 2000;
 const long VOLT_LOOP_TIME_ = 500;
 const long OPT_LOOP_TIME_ = 500;
 unsigned long last_t_task1 = 0;
@@ -92,6 +92,7 @@ const TickType_t _1s = 1000 / portTICK_PERIOD_MS;
 const TickType_t _1ms = 1 / portTICK_PERIOD_MS;
 const TickType_t _10ms = 10 / portTICK_PERIOD_MS;
 const TickType_t _100ms = 100 / portTICK_PERIOD_MS;
+const TickType_t _200ms = 200 / portTICK_PERIOD_MS;
 const TickType_t _5s = 5000 / portTICK_PERIOD_MS; 
 const TickType_t _10s = 10000 / portTICK_PERIOD_MS; 
 TaskHandle_t Task1;
@@ -102,7 +103,9 @@ class SensorValues{
     SensorValues(unsigned short array_size, unsigned short factor, short code){
       this->array_size = array_size;
       this->values = new int[array_size];
+//      this->timestamps = new uint32_t[array_size];
       std::fill_n(this->values,array_size,-200);
+//      std::fill_n(this->timestamps,array_size,-200);
       this->factor = factor;
       this->code = code;
     }
@@ -112,15 +115,31 @@ class SensorValues{
         if(this->index < this->array_size){
           is_writing = 1;
           this->values[index] = value*factor;
+//          this->timestamps[index] = time(NULL);
           this->index++;
           is_writing = 0;
         }
       }
     }
-    
+
+    void add_int_value(uint32_t value) volatile{
+      if(!is_reading){
+        if(this->index < this->array_size){
+          is_writing = 1;
+          this->values[index] = value*factor;
+          this->index++;
+          is_writing = 0;
+        }
+      }
+    }
+  
     float get_value_at_index(short index) volatile{
       return this->values[index];
     }
+
+//    uint32_t get_timestamp_at_index(short index) volatile{
+//      return this->timestamps[index];
+//    }
 
     short get_factor() volatile{
       return this->factor;
@@ -139,14 +158,16 @@ class SensorValues{
       }
       is_reading = 1;
       DynamicJsonDocument msg(1024);
+      msg["id"] = pkg_id;
       msg["code"] = this->code;
       msg["factor"] = this->factor;
-      msg["ts"] = time(NULL);
       JsonArray var_data = msg.createNestedArray("data");
+//      JsonArray tStamps = msg.createNestedArray("ts");
       for(int i=0;i<this->array_size;i++){
         if(this->values[i]==-200)
           break;
         var_data.add(this->values[i]);
+//        tStamps.add(this->timestamps[i]);
         this->values[i]=-200;
       }
       this->index = 0;
@@ -178,6 +199,7 @@ class SensorValues{
   private:
     volatile unsigned short index;
     volatile int *values;
+//    volatile uint32_t* timestamps;
     volatile unsigned short array_size;
     volatile unsigned short factor;
     char code;
@@ -235,7 +257,8 @@ void setup()
   delay(1000);
 
   //pinMode(OPTICAL_PIN, INPUT);
-  
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(250E3);
 }
 
 void loop()
@@ -248,14 +271,13 @@ void loop()
   Heltec.display->drawString(90, 0, String(counter));
   Heltec.display->display();
 
-//  Timestamps.add_value(millis());
+  Timestamps.add_int_value(time(NULL));
   voltageSensor();
   opticalSensor();
   accSensor();
   temperatureSensor();
   vTaskDelay( _100ms );
 //  vTaskDelay( _1s );
-//  //publish_all_values();
   UBaseType_t uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
   String mensagem = "Core:" + String((int)xPortGetCoreID()) + " --> Stack used: " + String((uint32_t)uxHighWaterMark);
   Serial.println(mensagem);
@@ -298,40 +320,6 @@ void config_rtc(){
   WiFi.mode(WIFI_OFF);
 }
 
-//void printLocalTime(){
-//  struct tm timeinfo;
-//  if(!getLocalTime(&timeinfo)){
-//    Serial.println("Failed to obtain time");
-//    return;
-//  }
-//  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-//  Serial.print("Day of week: ");
-//  Serial.println(&timeinfo, "%A");
-//  Serial.print("Month: ");
-//  Serial.println(&timeinfo, "%B");
-//  Serial.print("Day of Month: ");
-//  Serial.println(&timeinfo, "%d");
-//  Serial.print("Year: ");
-//  Serial.println(&timeinfo, "%Y");
-//  Serial.print("Hour: ");
-//  Serial.println(&timeinfo, "%H");
-//  Serial.print("Hour (12 hour format): ");
-//  Serial.println(&timeinfo, "%I");
-//  Serial.print("Minute: ");
-//  Serial.println(&timeinfo, "%M");
-//  Serial.print("Second: ");
-//  Serial.println(&timeinfo, "%S");
-//
-//  Serial.println("Time variables");
-//  char timeHour[3];
-//  strftime(timeHour,3, "%H", &timeinfo);
-//  Serial.println(timeHour);
-//  char timeWeekDay[10];
-//  strftime(timeWeekDay,10, "%A", &timeinfo);
-//  Serial.println(timeWeekDay);
-//  Serial.println();
-//}
-
 void temperatureSensor() {
   DS18B20.requestTemperatures();       // send the command to get temperatures
   tempC = DS18B20.getTempCByIndex(0);  // read temperature in °C
@@ -349,14 +337,8 @@ void setup_imu(){
 
 void accSensor() {
   int accX = getXAcc(ADXAddress);
-//  sendPacket(String(ACC_X_CODE) + " : " + String(accX));
-//  delay(DELAY_/3);
   int accY = getYAcc(ADXAddress);
-//  sendPacket(String(ACC_Y_CODE) + " : " + String(accY));
-//  delay(DELAY_/3);
   int accZ = getZAcc(ADXAddress);
-//  sendPacket(String(ACC_Z_CODE) + " : " + String(accZ));
-//  delay(DELAY_/3);
   Accx.add_value(accX);
   Accy.add_value(accY);
   Accz.add_value(accZ);
@@ -371,8 +353,6 @@ void opticalSensor() {
   long endtime = starttime;
   while((endtime-starttime)<=OPT_LOOP_TIME_){
     if(dt_vel){
-//      rotations = 60000000/dt_vel;
-//      sum_rotations += 60000000/dt_vel;
       _sum_dt += dt_vel;
       _count++;
       vTaskDelay( _10ms );
@@ -380,8 +360,7 @@ void opticalSensor() {
     }
     endtime = millis();
   }
-  //sendPacket(String(ROTATIONS_CODE) + " : " + String(rotacoes));
-//  Serial.print(_count); Serial.print("$");Serial.println(_sum_dt);
+
   if(_sum_dt) 
     rotations = 60000000/(_sum_dt/_count);
   _sum_dt = 0;
@@ -408,74 +387,21 @@ void voltageSensor() {
     endtime = millis();
   }
   readRms.publish();
-  //sendPacket(String(VOLTAGE_CODE) + " : " + String(540.0*readRms.rmsVal,2));
   Voltages.add_value(540.0*readRms.rmsVal);
 }
 
 void sendJson(){
-//  DynamicJsonDocument sensors_msg(2048);
-//  sensors_msg["type"]='s';
-//  JsonArray timestampMcu_data = sensors_msg.createNestedArray(String(TIME_CODE));
-//  JsonArray rpm_data = sensors_msg.createNestedArray(String(ROTATIONS_CODE));
-//  JsonArray voltage_data = sensors_msg.createNestedArray(String(VOLTAGE_CODE));
-//  JsonArray xAcc_data = sensors_msg.createNestedArray(String(ACC_X_CODE));
-//  JsonArray yAcc_data = sensors_msg.createNestedArray(String(ACC_Y_CODE));
-//  JsonArray zAcc_data = sensors_msg.createNestedArray(String(ACC_Z_CODE));
-//  JsonArray temperature_data = sensors_msg.createNestedArray(String(TEMPERATURE_CODE));
-//  JsonArray factors = sensors_msg.createNestedArray(String(FACTORS_CODE));
-//  factors[TIME_CODE] = Timestamps.get_factor();
-//  factors[ROTATIONS_CODE] = Rotations.get_factor();
-//  factors[VOLTAGE_CODE] = Voltages.get_factor();
-//  factors[ACC_X_CODE] = Accx.get_factor();
-//  factors[ACC_Y_CODE] = Accy.get_factor();
-//  factors[ACC_Z_CODE] = Accz.get_factor();
-//  factors[TEMPERATURE_CODE] = Temperatures.get_factor();
-//  for(int i=0;i<ARRAY_SIZE;i++){
-//    int res=0;
-////    res += addValue(Timestamps.get_value_at_index(i),timestampMcu_data);
-//    res += addValue(Rotations.get_value_at_index(i),rpm_data);
-//    res += addValue(Voltages.get_value_at_index(i),voltage_data);
-//    res += addValue(Accx.get_value_at_index(i),xAcc_data);
-//    res += addValue(Accy.get_value_at_index(i),yAcc_data);
-//    res += addValue(Accz.get_value_at_index(i),zAcc_data);
-//    res += addValue(Temperatures.get_value_at_index(i),temperature_data);
-//    if(res==0)
-//      break;
-//  }
-//
-//  String json;
-//  serializeJson(sensors_msg, json);
-//  sendPacket(json);
-//  Serial.println(json);
-//
-////  Timestamps.clear_array();
-//  Rotations.clear_array();
-//  Voltages.clear_array();
-//  Accx.clear_array();
-//  Accy.clear_array();
-//  Accz.clear_array();
-//  Temperatures.clear_array();
-//
-//  DynamicJsonDocument tstamps_msg(1024);
-//  tstamps_msg["type"]='t';
-//  JsonArray timestampMcu_data = tstamps_msg.createNestedArray(String(TIME_CODE));
-//  for(int i=0;i<ARRAY_SIZE;i++){
-//    int res=0;
-//    res = addValue(Timestamps.get_value_at_index(i),timestampMcu_data);
-//    if(res==0)
-//      break;
-//  }
-//
   publish_all_values();
+  pkg_id++;
 }
 
-int addValue(float value,JsonArray json_array){
-  if(value!=-200 && value!=-201){
-    json_array.add(value);
-    return 1; 
-  }
-  return 0;
-}
+//int addValue(float value,JsonArray json_array){
+//  if(value!=-200 && value!=-201){
+//    json_array.add(value);
+//    return 1; 
+//  }
+//  return 0;
+//}
 
 void sendPacket(String message) {
   // send packet
@@ -488,19 +414,21 @@ void sendPacket(String message) {
 }
 
 void publish_all_values(){
+  Timestamps.publish_values();
+  vTaskDelay(_200ms);
   Rotations.publish_values();
-  delay(DELAY_);
-  Temperatures.publish_values();
-  delay(DELAY_);
-  Accx.publish_values();
-  delay(DELAY_);
-  Accy.publish_values();
-  delay(DELAY_);
-  Accz.publish_values();
-  delay(DELAY_);
+  vTaskDelay(_200ms);
   Voltages.publish_values();
-  delay(DELAY_);
-  counter += 6; 
+  vTaskDelay(_200ms);
+  Accx.publish_values();
+  vTaskDelay(_200ms);
+  Accy.publish_values();
+  vTaskDelay(_200ms);
+  Accz.publish_values();
+  vTaskDelay(_200ms);
+  Temperatures.publish_values();
+  vTaskDelay(_200ms);
+  counter += 7; 
 }
 
 void logo()
